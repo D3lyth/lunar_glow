5dc791b9ddf554df17630ad0838d21b2a7d9cf76
Del <d3lyth@yahoo.co.uk>
2024-01-30 21:47:42 +0000

    Add adjust bag and remove bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
 
-
+# View Bag
 def view_bag(request):
     """ A view to return the shopping bag page """
 
     return render(request, 'bag/bag.html')
 
-
+# Add to bag
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
@@ -23,3 +24,34 @@ def add_to_bag(request, item_id):
 
     return redirect(redirect_url)
     
+
+# Adjust bag
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+# Remove items from bag
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        bag = request.session.get('bag', {})
+
+        if item_id in bag:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
