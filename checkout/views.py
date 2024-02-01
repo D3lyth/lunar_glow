5553f1f934197ff438000cf4236a93f3df58f3bb
Del <d3lyth@yahoo.co.uk>
2024-02-01 22:30:58 +0000

    Remove size information from for loop

--- checkout/views.py
+++ checkout/views.py
@@ -47,7 +47,6 @@ def checkout(request):
                                 order=order,
                                 product=product,
                                 quantity=quantity,
-                                product_size=size,
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
