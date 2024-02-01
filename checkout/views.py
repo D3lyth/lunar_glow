1b9b03a450de4696cda3ececa863b1b71aa935f6
Del <d3lyth@yahoo.co.uk>
2024-02-01 14:12:49 +0000

    Add stripe elements and stripe css

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OeyPoHcht9xmJRkRjDJu4RY0S7bIN354UghoMlj2wrMmtUIhwmKNKnvEEfkF3zez36CFLhuc5PCAqAVZaAvr9G500hwQ181pR',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
