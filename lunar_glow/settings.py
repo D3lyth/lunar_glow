5344b9366ba3bc838d80301cacd9ee051ecf2d21
Del <d3lyth@yahoo.co.uk>
2024-02-10 19:26:48 +0000

    Deployment to heroku

--- lunar_glow/settings.py
+++ lunar_glow/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
+
 from pathlib import Path
 if os.path.exists('env.py'):
     import env
@@ -26,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lunar-glow-77444c493d2e.herokuapp.com', 'localhost']
 CSRF_TRUSTED_ORIGINS = []
 host = os.environ.get('SITE_NAME')
 if host:
@@ -123,13 +125,17 @@ WSGI_APPLICATION = 'lunar_glow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
