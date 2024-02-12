d8d0abe27e2e1ecea52494e307af62dae8c3beac
Del <d3lyth@yahoo.co.uk>
2024-02-12 23:45:50 +0000

    Create faqs model, views, templates and json

--- faqs/models.py
+++ faqs/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
-class FAQ(models.Model):
-    FAQ_CATEGORY_CHOICES = [
-        ('PRODUCTS', 'Products'),
-        ('ORDERS_DELIVERY', 'Orders and Delivery'),
-        ('CONTACT_US', 'Contact Us'),
-    ]
+class FAQCategory(models.Model):  # Renamed for better naming convention
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class FAQ_QA(models.Model):
+    faq_category = models.ForeignKey(FAQCategory, null=True, blank=True, on_delete=models.SET_NULL)
     question = models.CharField(max_length=255)
     answer = models.TextField()
-    category = models.CharField(max_length=20, choices=FAQ_CATEGORY_CHOICES)
-    created_at = models.DateTimeField(auto_now_add=True)  # Importing and using DateTimeField
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.question
+
+
 
--- faqs/views.py
+++ faqs/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import FAQ  
+from .models import FAQ_QA  
 from django.urls import reverse 
 
 def all_faqs(request):
     url = reverse('faqs')
-    faqs = FAQ.objects.all()  
+    faqs = FAQ_QA.objects.all()  
     return render(request, 'faqs/faqs.html', {'faqs': faqs})
 
