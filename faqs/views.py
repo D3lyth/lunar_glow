8c8a7689ae07f04d88093d1e829aa2b54ee50515
Del <d3lyth@yahoo.co.uk>
2024-02-13 00:58:59 +0000

    Add context to views

--- faqs/views.py
+++ faqs/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from .models import FAQ_QA  
-from django.urls import reverse 
+from .models import FAQCategory, FAQ_QA
 
 def all_faqs(request):
-    url = reverse('faqs')
-    faqs = FAQ_QA.objects.all()  
-    return render(request, 'faqs/faqs.html', {'faqs': faqs})
+    # Fetch all FAQ categories
+    categories = FAQCategory.objects.all()
+
+    # Fetch all FAQs
+    faqs = FAQ_QA.objects.all()
+
+    # Create a dictionary to hold categories and their associated FAQs
+    categories_with_faqs = {}
+
+    # Populate the dictionary with categories and their associated FAQs
+    for category in categories:
+        # Filter FAQs belonging to the current category
+        category_faqs = faqs.filter(faq_category=category)
+        categories_with_faqs[category] = category_faqs
+
+    # Pass the dictionary to the template context
+    context = {
+        'categories_with_faqs': categories_with_faqs
+    }
+
+    return render(request, 'faqs/faqs.html', context)
 
