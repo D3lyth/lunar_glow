5f6d5b00d12558e56c633dcba0682566054c1d9d
Del <d3lyth@yahoo.co.uk>
2024-02-13 16:25:41 +0000

    Add admin classes for superuser admin panel

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import FAQCategory, FAQ_QA
 
-# Register your models here.
+# Models
+
+class FAQCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class FAQ_QAAdmin(admin.ModelAdmin):
+    list_display = (
+        'faq_category', 
+        'question',
+        'answer',
+        )
+
+admin.site.register(FAQCategory, FAQCategoryAdmin)
+admin.site.register(FAQ_QA, FAQ_QAAdmin)
