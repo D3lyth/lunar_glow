5d25a76fe57308e2bcbbdad5a79b9371e49fbbf6
Del <d3lyth@yahoo.co.uk>
2024-02-13 22:44:43 +0000

    Add registration of model on django admin panel

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Subscriber
 
-# Register your models here.
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'timestamp')
+    search_fields = ('email', 'name')
+    list_filter = ('timestamp',)
+
+# Register the Subscriber model with the admin site
+admin.site.register(Subscriber, SubscriberAdmin)
