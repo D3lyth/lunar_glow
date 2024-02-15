b9a66808db4c1c7722037faf3f4ef5e21cdf519f
Del <d3lyth@yahoo.co.uk>
2024-02-15 17:16:12 +0000

    Refactor code to allow form validation

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import SubscriberForm
 
+
 def sign_up(request):
+    form = SubscriberForm(request.POST or None) 
     if request.method == 'POST':
-        form = SubscriberForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('thank_you')
-    else:
-        form = SubscriberForm()
+
     return render(request, 'newsletter/sign_up.html', {'form': form})
+
 
 def thank_you(request):
     return render(request, 'newsletter/thank_you.html')
