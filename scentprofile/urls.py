45b580b70a35646cd0b3f2729426968c65eb6622
Del <d3lyth@yahoo.co.uk>
2024-02-17 00:47:45 +0000

    Add urls for scent profile

---
+++ scentprofile/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+from .views import scent_selection_view
+
+urlpatterns = [
+    path('', views.scent_profile_view, name='scentprofile'),
+    # path('scentprofile/', views.scent_selection_view, name='scentselection'),
+    path('scentprofile/<str:scent_profile>/', views.scent_selection_view, name='scentselection'),
+
+
+]
