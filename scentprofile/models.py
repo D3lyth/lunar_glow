dcb6cf6d97f7d6bd2f5385245cd869648eab11f5
Del <d3lyth@yahoo.co.uk>
2024-02-18 12:19:41 +0000

    Update model to link tp product model

--- scentprofile/models.py
+++ scentprofile/models.py
@@ -12,6 +12,7 @@ class ProductScent(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     scentoptions = models.ForeignKey(ScentProfile, on_delete=models.CASCADE, null=True, blank=True)
+    products = models.ManyToManyField('products.Product')
 
     def __str__(self):
         return self.name
