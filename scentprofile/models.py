a874d8524b5814917e2b554505596208d0b4265b
Del <d3lyth@yahoo.co.uk>
2024-02-22 10:09:52 +0000

    Remove unused model and refactor code

--- scentprofile/models.py
+++ scentprofile/models.py
@@ -1,18 +1,9 @@
 from django.db import models
 
+
 class ScentProfile(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     
-    def __str__(self):
-        return self.name
-
-
-class ProductScent(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    scentoptions = models.ForeignKey(ScentProfile, on_delete=models.CASCADE, null=True, blank=True)
-    products = models.ManyToManyField('products.Product')
-
     def __str__(self):
         return self.name
