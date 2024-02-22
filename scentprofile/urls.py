3386128d5dafaa5067ec9814554f691ab87037b0
Del <d3lyth@yahoo.co.uk>
2024-02-22 10:10:10 +0000

    Remove unused url and views

--- scentprofile/urls.py
+++ scentprofile/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views import scent_selection_view
 
 urlpatterns = [
     path('', views.scent_profile_choices, name='scentprofile'),
-    path('scentprofile/<int:scentprofile_id>/', views.scent_selection_view, name='scentselection'),
 ]
